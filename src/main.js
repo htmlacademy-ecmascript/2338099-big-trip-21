a7091f0f4da4d0a9145f2d8ac11c15c4001eb7f2
Alexander <papaburgerr@gmail.com>
2023-08-17 16:37:22 +1000

    ошибка

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import PointView from './view/point-view.js';
-import { render, } from './render.js';
+import FilterView from './view/trip-filters.js';
+import { render,RenderPosition } from './render.js';
 
-const pointElement = document.querySelector('.trip-main');
+const pointElement = document.querySelector('.trip-events');
+const filterElement = document.querySelector('.trip-main');
 
-
-render(new PointView, pointElement);
+render(new PointView, pointElement, RenderPosition.BEFOREEND);
+render(new FilterView, filterElement);
