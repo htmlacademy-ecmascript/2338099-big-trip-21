49bc0d6c7dd7db9c6d39adcdc9d0f9ee233fa838
Alexander <papaburgerr@gmail.com>
2023-08-20 12:08:14 +1000

    Исправлен

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -5,19 +5,18 @@ import NewTripSortingtView from '../view/trip-sorting';
 import { render } from '../render';
 
 export default class NewTripPresenter {
-  tripSortingComponent = new NewPointListView();
+  tripListComponent = new NewPointListView();
 
   constructor({tripContainer}) {
     this.tripContainer = tripContainer;
   }
 
   init() {
-    render(this.tripSortingComponent,this.tripContainer);
+    render(this.tripListComponent, this.tripContainer);
     const pointListElement = this.tripContainer.querySelector('.trip-events__list');
-    const editingFormElement = this.tripContainer.querySelector('.trip-events__list');
 
-    render(new NewTripSortingtView(),editingFormElement);
-    render(new NewEditingFormtView(), editingFormElement);
+    render(new NewTripSortingtView(),pointListElement);
+    render(new NewEditingFormtView(), pointListElement);
 
     for(let i = 0; i < 3; i++) {
       render(new NewPoinView(), pointListElement);
