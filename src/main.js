787c11814b921bb5e4af098b1106a3e6ff55abef
Alexander <papaburgerr@gmail.com>
2023-08-21 15:04:26 +1000

    1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import TripInfoView from './view/trip-info-view.js';
 import TripfilterView from './view/trip-filters-view.js';
-import PointListView from './view/list_view.js';
 import { render, RenderPosition } from './render.js';
 import TripPresenter from './presenter/trip-presenter.js';
 
@@ -12,7 +11,6 @@ const tripFilterElement = document.querySelector('.trip-controls__filters');
 
 render(new TripInfoView(), tripElement,RenderPosition.AFTERBEGIN);
 render(new TripfilterView(), tripFilterElement);
-render(new PointListView(), tripContainer);
 
 const tripPresenter = new TripPresenter({tripContainer});
 
