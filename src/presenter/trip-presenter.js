2c388c085ea3411ac6e8e354aaf878e87d29019d
Alexander <papaburgerr@gmail.com>
2023-08-21 17:32:54 +1000

    домашка

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,8 +1,8 @@
-import PointView from '../view/Point-view';
-import PointListView from '../view/list_view';
+import PointListView from '../view/point-list-view';
+import PointView from '../view/point-view';
 import EditingFormtView from '../view/editing-form-view';
 import TripSortingtView from '../view/trip-sorting-view';
-import { render, RenderPosition } from '../render';
+import { render, } from '../render';
 
 export default class TripPresenter {
   tripListComponent = new PointListView();
@@ -12,14 +12,13 @@ export default class TripPresenter {
   }
 
   init() {
+    render(new TripSortingtView(), this.tripContainer);
     render(this.tripListComponent, this.tripContainer,);
-    const pointListElement = this.tripContainer.querySelector('.trip-events__list');
 
-    render(new TripSortingtView(),this.tripContainer,RenderPosition.AFTERBEGIN);
-    render(new EditingFormtView(), pointListElement);
+    render(new EditingFormtView(), this.tripListComponent.getElement());
 
     for(let i = 0; i < 3; i++) {
-      render(new PointView(), pointListElement);
+      render(new PointView(), this.tripListComponent.getElement());
     }
   }
 }
