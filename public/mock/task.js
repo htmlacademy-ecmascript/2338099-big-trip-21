17ab967f5e4196bd2f1595204410cc5939aff890
Alexander <papaburgerr@gmail.com>
2023-08-21 17:23:21 +1000

    Готовая домашка

---
+++ public/mock/task.js
@@ -0,0 +1,17 @@
+import { CITIES, } from "../../src/const"
+import { getRandomArrayElement } from "../../src/utils"
+
+const mockTasks = [
+  pointType: {
+    taxi: trye,
+    bus: trye,
+    train: trye,
+    ship: trye,
+    drive: trye,
+    flight: trye,
+    checkIn: trye,
+    Sightseeing: trye,
+    Restaurant: trye,
+},
+destination: getRandomArrayElement(CITIES),
+]
---
+++ src/const.js
@@ -0,0 +1,3 @@
+const CITIES = ['Moscow', 'Rostov', 'Ufa', 'Kazan', 'Khabarovsk'];
+
+export {CITIES};
